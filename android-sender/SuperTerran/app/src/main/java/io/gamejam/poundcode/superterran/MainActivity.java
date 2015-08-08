2845cce98e9c8b4f1d2380513f53f13939125a04
chris_pound <c2.pound@gmail.com>
2015-08-08 01:07:08 -0500

    added main activity

---
+++ android-sender/SuperTerran/app/src/main/java/io/gamejam/poundcode/superterran/MainActivity.java
@@ -0,0 +1,16 @@
+package io.gamejam.poundcode.superterran;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by chris_pound on 8/8/15.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
